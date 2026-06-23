723c01539cf00adcaffc77929bae2b050a5c5cad
PrismID <dito.hdez@gmail.com>
2026-06-23 12:06:17 -0400

    Update vi.js

--- Vehicle Interface (VI)/vi.js
+++ Vehicle Interface (VI)/vi.js
@@ -1,10 +1,12 @@
 // vi.js - The Vehicle OS Controller
 
 // 1. PWA Registration
+/*
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
         .then(() => console.log('VI: Service Worker Registered'));
 }
+*/
 
 // 2. Connectivity Monitor (Crucial for the driver)
 window.addEventListener('online', () => {
