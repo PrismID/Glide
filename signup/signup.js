8b5d0e2e63efa796fdb2c8c24664fd39c215f4d1
PrismID <dito.hdez@gmail.com>
2026-08-14 18:52:19 -0400

    Update signup.js

--- signup/signup.js
+++ signup/signup.js
@@ -4,15 +4,6 @@ import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 // 2. Initialize in the same file
-const firebaseConfig = {
-    apiKey: "AIzaSyA1owZzydT29hztK74bMNQvskezuOG8uZ8",
-    authDomain: "glide-rideshares.firebaseapp.com",
-    projectId: "glide-rideshares",
-    storageBucket: "glide-rideshares.firebasestorage.app",
-    messagingSenderId: "936136981965",
-    appId: "1:936136981965:web:bdbcc48520577704281884",
-    measurementId: "G-9LZMHZ8351"
-};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
